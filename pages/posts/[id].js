da6448ac46a63b9f8267c30c73bca938fbbde07d
Ach Rozikin@MacBook <geronimo794@gmail.com>
2022-07-04 13:11:36 +0700

    Dynamic link

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,25 +1,33 @@
 import Layout from '../../components/layout';
+import Date from '../../components/date';
+import utilStyles from '../../styles/utils.module.css';
 import { getAllPostIds, getPostData } from '../../lib/posts';
+import Head from 'next/head';
 
 export async function getStaticProps({ params }) {
-	const postData = getPostData(params.id);
+	const postData = await getPostData(params.id);
 	return {
-	  props: {
-		postData,
-	  },
+		props: {
+			postData,
+		},
 	};
-  }
-  export default function Post({ postData }) {
-	  return (
+}
+export default function Post({ postData }) {
+	return (
 		<Layout>
-		  {postData.title}
-		  <br />
-		  {postData.id}
-		  <br />
-		  {postData.date}
+			<Head>
+				<title>{postData.title}</title>
+			</Head>
+			<article>
+				<h1 className={utilStyles.headingXl}>{postData.title}</h1>
+				<div className={utilStyles.lightText}>
+					<Date dateString={postData.date} />
+				</div>
+				<div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+			</article>
 		</Layout>
-	  );
-	}
+	);
+}
 export async function getStaticPaths() {
 	const paths = getAllPostIds();
 	return {
